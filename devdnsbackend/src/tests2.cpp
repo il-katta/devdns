0d1822199461b28e77cf42be9552e0c75421d724
Andrea Cattaneo <andrea.cattaneo.dev@gmail.com>
2022-09-30 14:09:48 +0200

    trying to use uacme library

--- devdnsbackend/src/tests2.cpp
+++ devdnsbackend/src/tests2.cpp
@@ -9,6 +9,7 @@
 extern "C" {
 #include "uacme/uacme.c"
 #include "uacme/msg.h"
+#include "uacme/curlwrap.h"
 }
 
 bool file_exists(const std::string &filepath) {
@@ -16,31 +17,63 @@ bool file_exists(const std::string &filepath) {
 }
 
 extern "C" {
+acme_t a;
+
+void _out(char *csr, char *keyname, char **names) {
+    free(keyname);
+    json_free(a.json);
+    json_free(a.account);
+    json_free(a.dir);
+    json_free(a.order);
+    free(a.nonce);
+    free(a.kid);
+    free(a.headers);
+    free(a.body);
+    free(a.type);
+    free(a.keyprefix);
+    free(a.certprefix);
+    //if (a.key)
+    //    privkey_deinit(a.key);
+    //if (key)
+    //    privkey_deinit(key);
+    free(csr);
+    crypto_deinit();
+    //curl_global_cleanup();
+    if (names) {
+        for (int i = 0; names && names[i]; i++)
+            free(names[i]);
+        free(names);
+    }
+}
+
+
 int main() {
-    acme_t a;
     memset(&a, 0, sizeof(a));
     keytype_t key_type = PK_RSA;
     int key_bits = 2048;
     a.email = "test.acme@loopback.it";
-    std::vector<const char*> names = {"devdns.loopback.it", "www.devdns.loopback.it"};
+    std::vector<const char *> names = {"devdns.loopback.it", "www.devdns.loopback.it"};
     names.push_back(nullptr);
     names.shrink_to_fit();
+    char ** _names = const_cast<char **>(names.data());
 
     g_loglevel = 9;
     a.directory = STAGING_URL;
 
-    char*keyname ;
+    char *keyname;
     asprintf(&keyname, "%s.key", names[0]);
 
     std::cout << "acme_bootstrap ..." << std::endl;
     if (!acme_bootstrap(&a)) {
         std::cout << "acme_bootstrap fails" << std::endl;
-    } else {
-        std::cout << "acme_bootstrap ok" << std::endl;
+        _out(nullptr, keyname, _names);
+        return 1;
     }
+    std::cout << "acme_bootstrap ok" << std::endl;
 
     if (file_exists(keyname)) {
         a.key = key_load(key_type, key_bits, keyname);
+        account_retrieve(&a);
     } else {
         a.key = key_load(key_type, key_bits, keyname);
         std::cout << "account_new ..." << std::endl;
@@ -51,14 +84,20 @@ int main() {
         }
     }
 
-    free(keyname);
-
-    auto csr = csr_gen(const_cast<char *const *>(names.data()), false, a.key);
-    if (csr) {
-        std::cout << fmt::format("hello {}", csr) << std::endl;
-        return 0;
-    } else {
+    auto csr = csr_gen(_names, false, a.key);
+    if (!csr) {
+        std::cout << "csr_gen fail" << std::endl;
+        _out(csr, keyname, _names);
         return 1;
     }
+    if (!cert_issue(&a, _names, csr)) {
+        std::cout << "cert_issue fail" << std::endl;
+        _out(csr, keyname, _names);
+        return 1;
+    }
+    std::cout << fmt::format("hello {}", csr) << std::endl;
+    return 0;
 }
+
+
 }
